20e43cbae4966a085442aa0c763b371ad8d9b023
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-09-12 15:08:01 +0300

    0.2.9

---
+++ new_project/src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/css/main.css";
+class App extends React.Component {
+  render() {
+    return <div style={{ fontSize: 50 }}>Hello 👋</div>;
+  }
+}
+
+export default App;
