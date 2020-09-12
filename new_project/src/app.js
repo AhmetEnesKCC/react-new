fab59092da9a36162a32645f172f99af3abba7c5
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-09-12 15:09:36 +0300

    deleted new project

--- new_project/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./styles/css/main.css";
-class App extends React.Component {
-  render() {
-    return <div style={{ fontSize: 50 }}>Hello 👋</div>;
-  }
-}
-
-export default App;
