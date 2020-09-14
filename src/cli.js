a33ad903bec662678a1603c0f30515a8bad4b934
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2020-09-14 22:47:12 +0300

    added every file to testing

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,12 @@
 import arg from "arg";
 import inquirer from "inquirer";
 // import CLI from "clui";
-import { createNpmFile, createManifestJson, createReactNewAppConfigJSON, addTemplate } from "../methods/execCommands";
+import {
+  createNpmFile,
+  createManifestJson,
+  createReactNewAppConfigJSON,
+  addTemplate
+} from "../methods/execCommands";
 import path from "path";
 import clear from "clear";
 import chalk from "chalk";
@@ -14,26 +19,34 @@ import Listr from "listr";
 import { title } from "process";
 import figlet from "figlet";
 import templates from "../templates.json";
+import OS from "os";
+
 const execa = require("execa");
 var globalValue = "New_Project";
 const mustInstallPackages = [];
 var packages = {
   react: ["react", "react-dom", "react-scripts"],
-  sass: ["gulp-sass", "gulp", "gulp-autoprefixer", "gulp-minify-css", "gulp-sourcemaps"],
+  sass: [
+    "gulp-sass",
+    "gulp",
+    "gulp-autoprefixer",
+    "gulp-minify-css",
+    "gulp-sourcemaps"
+  ],
   redux: ["redux", "react-redux", "redux-thunk"],
   router: ["redux", "react-redux", "redux-thunk"],
   bootstrap: ["bootstrap", "react-bootstrap"],
   fontawesome: [
     "@fortawesome/fontawesome-svg-core",
     "@fortawesome/free-solid-svg-icons",
-    "@fortawesome/react-fontawesome",
-  ],
+    "@fortawesome/react-fontawesome"
+  ]
 };
 mustInstallPackages.push(...packages.react);
 var saveAsTemplate = false;
 const checkInternetConnected = require("check-internet-connected");
 const templatesArray = [];
-templates.map((template) => {
+templates.map(template => {
   templatesArray.push(template.name);
 });
 // check internet connection
@@ -42,7 +55,9 @@ templates.map((template) => {
 clear();
 
 console.log("\n");
-console.log(chalk.green(figlet.textSync("React New App", { horizontalLayout: "full" })));
+console.log(
+  chalk.green(figlet.textSync("React New App", { horizontalLayout: "full" }))
+);
 console.log("\n");
 var templateWillBe = true;
 var selected_template = "";
@@ -55,7 +70,7 @@ async function promptForTemplate(options) {
       name: "template",
       message: "Do you want to use template if not no select one from above.",
       choices: ["No", new inquirer.Separator()].concat(templatesArray),
-      default: defaultTemplate,
+      default: defaultTemplate
     });
   } else {
     return;
@@ -64,7 +79,7 @@ async function promptForTemplate(options) {
   const answers = await inquirer.prompt(questions);
   return {
     ...options,
-    template: options.template || answers.template,
+    template: options.template || answers.template
   };
 }
 
@@ -83,14 +98,17 @@ async function promptForMissingOptions(options) {
   const defaultBase = "Function";
   var noTemplate = true;
   const questions = [];
-  const autoFix = (string) => {
+  const autoFix = string => {
     string = string.trim();
     for (let i = 0; i < string.length; i++) {
       if (string[i] === " " && string[i + 1] === " ") {
         string = string.slice(i, 1);
       }
     }
-    string = string.replace("-", "_").replace(" ", "_").toLowerCase();
+    string = string
+      .replace("-", "_")
+      .replace(" ", "_")
+      .toLowerCase();
     return string;
   };
 
@@ -99,11 +117,12 @@ async function promptForMissingOptions(options) {
     name: "project_name",
     message: "Please enter a valid name for your new react project",
     default: "New Project",
-    validate: async function (value) {
+    validate: async function(value) {
       if (value.length > 0) {
         if (
           fs.readdirSync(process.cwd()).indexOf(value) >= 0 ||
-          (fs.readdirSync(process.cwd()).indexOf("new_project") >= 0 && value === "New Project")
+          (fs.readdirSync(process.cwd()).indexOf("new_project") >= 0 &&
+            value === "New Project")
         ) {
           return "There is already a folder named : " + autoFix(value);
         }
@@ -119,7 +138,7 @@ async function promptForMissingOptions(options) {
       } else {
         return "Please Enter a valid name";
       }
-    },
+    }
   });
   if (templateWillBe === false || templates.length === 0) {
     questions.push({
@@ -127,20 +146,27 @@ async function promptForMissingOptions(options) {
       name: "base",
       message: "Do you want function based or class based component ?",
       choices: ["Class", "Function"],
-      default: defaultBase,
+      default: defaultBase
     });
 
     questions.push({
       type: "checkbox",
       name: "options",
       message: "please select which one do you want to use",
-      choices: ["redux", "sass", "react-router", "bootstrap", "font awesome 5" /*, "typescript"*/], // will add TS later
+      choices: [
+        "redux",
+        "sass",
+        "react-router",
+        "bootstrap",
+        "font awesome 5" /*, "typescript"*/
+      ] // will add TS later
     });
     questions.push({
       type: "list",
       name: "save",
-      message: "Select yes if you want to save your template. ( Template name will ask when finish )",
-      choices: ["Yes", "No"], // will add TS later
+      message:
+        "Select yes if you want to save your template. ( Template name will ask when finish )",
+      choices: ["Yes", "No"] // will add TS later
     });
   }
 
@@ -151,11 +177,11 @@ async function promptForMissingOptions(options) {
     redux: options.redux || answers.redux,
     options: options.options || answers.options,
     template: options.use_template || answers.use_template,
-    save: options.save || answers.save,
+    save: options.save || answers.save
   };
 }
 let new_template;
-
+var yarn = true;
 export async function cli(args) {
   let options;
   options = await promptForMissingOptions(args);
@@ -165,7 +191,7 @@ export async function cli(args) {
   new_template = options;
   var project_name = options.project_name;
   if (templateWillBe === true && templates.length > 0) {
-    options = templates.find((temp) => temp.name === selected_template);
+    options = templates.find(temp => temp.name === selected_template);
     options.project_name = project_name;
     new_template = options;
   }
@@ -184,47 +210,70 @@ export async function cli(args) {
   if (options.options.includes("font awesome 5")) {
     mustInstallPackages.push(...packages.fontawesome);
   }
-
+  const checkingOs = new Listr([
+    {
+      title: "Checking OS",
+      task: () => {
+        let ostype = OS.type();
+        console.log("Your OS is : " + ostype);
+      }
+    }
+  ]);
+  await checkingOs.run().catch(err => console.log(err));
   const testConnection = new Listr([
     {
       title: "Checking internet connection",
       task: () => {
         checkInternetConnected()
-          .then((res) => {
+          .then(res => {
             console.log("internet connection stable.");
           })
-          .catch((ex) => {
-            console.log(chalk.red("\nNo internet connection. Please turn on your internet.\n"));
-            console.log(chalk.green("\nDo not worry your settings saved as <no internet> :D.\n"));
+          .catch(ex => {
+            console.log(
+              chalk.red(
+                "\nNo internet connection. Please turn on your internet.\n"
+              )
+            );
+            console.log(
+              chalk.green(
+                "\nDo not worry your settings saved as <no internet> :D.\n"
+              )
+            );
 
             process.exit();
           });
-      },
-    },
+      }
+    }
   ]);
-  await testConnection.run().catch((err) => console.log(err));
+  await testConnection.run().catch(err => console.log(err));
   const testYarn = new Listr([
     {
       title: "Testing for yarn",
       task: (ctx, task) =>
         execa("yarn").catch(() => {
           ctx.yarn = false;
+          yarn = false;
           task.skip("Yarn is not installed. Installing now");
-        }),
+        })
     },
     {
       title: "Installing Yarn",
-      enabled: (ctx) => ctx.yarn === false,
-      task: () => execa("npm", ["install", "yarn", "-g"]),
-    },
+      enabled: !yarn,
+      task: () => {
+        execa("npm", ["install", "yarn", "-g"]);
+      }
+    }
   ]);
-  testYarn.run().catch((err) => console.log(err));
+  await testYarn.run().catch(err => console.log(err));
   let packageArray = [];
 
-  let installPackages = mustInstallPackages.map((pack) => {
+  let installPackages = mustInstallPackages.map(pack => {
     packageArray.push({
       title: "Installing " + pack,
-      task: () => execa("yarn", ["add", pack], { cwd: path.join(process.cwd(), options.project_name) }),
+      task: () =>
+        execa("yarn", ["add", pack], {
+          cwd: path.join(process.cwd(), options.project_name)
+        })
     });
   });
   var installedVersion = "";
@@ -232,12 +281,12 @@ export async function cli(args) {
 
   var Latest = true;
   let isReactNewAppLatest = () => {
-    execa("npm", ["list", "react-new-app", "-g"]).catch((result) => {
+    execa("npm", ["list", "react-new-app", "-g"]).catch(result => {
       var index = result.indexOf("react-new-app");
       installedVersion = result.slice(index + 13, index + 18);
       console.log(installedVersion);
     });
-    execa("npm", ["view", "react-new-app", "version"]).catch((result) => {
+    execa("npm", ["view", "react-new-app", "version"]).catch(result => {
       latestVersion = result;
       console.log(latestVersion);
     });
@@ -256,49 +305,55 @@ export async function cli(args) {
               title: "copying folders and files",
               task: async () => {
                 return createPureReact(options);
-              },
+              }
             },
             {
               title: "creating package.json",
               task: async () => {
                 createNpmFile(options, mustInstallPackages);
                 createManifestJson(options);
-              },
+              }
             },
             {
               title: "Checking version of React New App",
               task: () => {
-                execa("npm", ["list", "react-new-app", "-g"]).then((result) => {
+                execa("npm", ["list", "react-new-app", "-g"]).then(result => {
                   if (result.stdout !== "") {
                     var index = result.stdout.indexOf("react-new-app");
-                    installedVersion = result.stdout.slice(index + 14, index + 19);
+                    installedVersion = result.stdout.slice(
+                      index + 14,
+                      index + 19
+                    );
                   }
                 });
-                execa("npm", ["view", "react-new-app", "version"]).then((result) => {
-                  latestVersion = result.stdout;
-                });
+                execa("npm", ["view", "react-new-app", "version"]).then(
+                  result => {
+                    latestVersion = result.stdout;
+                  }
+                );
 
                 if (latestVersion !== installedVersion) {
                   Latest = false;
                 }
-              },
+              }
             },
             {
               title: "Creating react_new_app.config.json",
-              task: async () => createReactNewAppConfigJSON(options, installedVersion),
-            },
+              task: async () =>
+                createReactNewAppConfigJSON(options, installedVersion)
+            }
           ],
           { concurrent: true }
         );
-      },
+      }
     },
 
     {
       title: "Installing Dependencies",
-      task: () => new Listr(packageArray),
-    },
+      task: () => new Listr(packageArray)
+    }
   ]);
-  await tasks.run().catch((err) => console.log(err));
+  await tasks.run().catch(err => console.log(err));
 
   console.log(chalk.yellow("\nThank you for used react-new"));
   console.log(chalk.green("\nRecommended"));
@@ -313,7 +368,12 @@ export async function cli(args) {
   if (!Latest) {
     console.log(chalk.yellow("\n\nThere is new version of react-new-app"));
     console.log("\n" + installedVersion + " --> " + latestVersion);
-    console.log("Please update with -> " + "npm " + chalk.red("update ") + "react-new-app -g");
+    console.log(
+      "Please update with -> " +
+        "npm " +
+        chalk.red("update ") +
+        "react-new-app -g"
+    );
   }
 
   // execCommands("npm init -y");
@@ -328,22 +388,22 @@ async function promptForTemplateName(options) {
       name: "template_name",
       message: "Please enter your template name",
       default: defaultName,
-      validate: (value) => {
+      validate: value => {
         var temp_names = [];
-        templates.map((temp) => {
+        templates.map(temp => {
           temp_names.push(temp.name);
         });
         if (temp_names.includes(value)) {
           return "There are already named template : " + value;
         }
         return true;
-      },
+      }
     });
   }
   const answers = await inquirer.prompt(questions);
   return {
     ...options,
-    name: options.template_name || answers.template_name,
+    name: options.template_name || answers.template_name
   };
 }
 
@@ -354,10 +414,10 @@ export async function save_as_template(args) {
     const templateNameTask = new Listr([
       {
         title: "Adding Template",
-        task: () => addTemplate(new_template),
-      },
+        task: () => addTemplate(new_template)
+      }
     ]);
-    await templateNameTask.run().catch((err) => {
+    await templateNameTask.run().catch(err => {
       console.log(err);
     });
   }
